7980f491cfcbe4d2da5a74e0425d3bfa3f84007a
Ryan Spice : Book <spice.ryan@hotmail.com>
2023-01-02 11:22:21 -0500

    contact

--- src/lib/components/header/Contact.svelte
+++ src/lib/components/header/Contact.svelte
@@ -24,61 +24,57 @@
   export let enabledEmail = true;
   export let enabledPhone = true;
   export let enabledShare = true;
-</script>
 
-<MediaQuery
-  query={[
-    "(max-width: 768px)",
-    "(min-width: 768px) and (max-width: 1280px)",
-    "(min-width: 1280px)",
-  ]}
-  let:matches
->
+  export let align = '';
+  let order = [
+    4,
+    3,
+    0,
+    5,
+    5,
+  ];
+
+</script>
+<MediaQuery query={['(max-width: 768px)', '(min-width: 768px) and (max-width: 1280px)' , '(min-width: 1280px)' ]}
+  let:matches>
   {@const [mobile, tablet, desktop] = matches}
-  <div class="container">
+  <div class=''>
     <!-- -->
-    <div class="">
-      <ul class="top-bar-info">
-        <li />
-        {#if enabledEmail}
-          <li>
-            <ExternalLink
-              href={"mailto:" + website.siteContactEmail}
-              icon="email"
-              label={desktop ? website.siteContactEmail : ""}
-              alt="Send Email"
-            />
-          </li>
+    <div class=''>
+      <ul class={'top-bar-info '+align}>
+        <li></li>
+        <li></li>
+        <li></li>
+        {#if (enabledEmail)}
+        <li style={"order:"+order[0]+";"}>
+          <ExternalLink href={"mailto:"+website.siteContactEmail} icon="email"
+            label={desktop?website.siteContactEmail:''} alt="Send Email" />
+        </li>
         {/if}
-        {#if enabledPhone}
-          <li>
-            <ExternalLink
-              href={"tel:" + website.siteContactPhone}
-              icon="phone"
-              label={desktop ? website.siteContactPhone : ""}
-              alt="Make a Call"
-            />
-          </li>
+        {#if (enabledPhone)}
+        <li style={"order:"+order[1]+";"}>
+          <ExternalLink href={"tel:"+website.siteContactPhone} icon="phone" label={desktop?website.siteContactPhone:''}
+            alt="Make a Call" />
+        </li>
         {/if}
-        {#if enabledFacebook}
-          <li>
-            <ExternalLink
-              href={"https://" + website.siteContactFacebook}
-              icon="facebook"
-              alt="Facebook"
-              style="top:3px;transform: scale(1.3);"
-            />
-          </li>
+        {#if (enabledFacebook)}
+        <li style={"order:"+order[2]+";"}>
+          <ExternalLink href={"https://"+website.siteContactFacebook} icon="facebook" alt="Facebook"
+            style="top:3px;transform: scale(1.3);" />
+        </li>
         {/if}
-        {#if enabledShare}
+        {#if (enabledShare)}
+        <li style={"order:"+order[3]+";"}>
+
           <ExternalLinkShare />
+        </li>
         {/if}
-        {#if enabledLinkedin}
-          <li>
-            <ExternalLink href={website.siteContactLinkedIn} alt="LinkedIn">
-              <LinkedIn />
-            </ExternalLink>
-          </li>
+        {#if (enabledLinkedin)}
+        <li style={"order:"+order[4]+";"}>
+          <ExternalLink href={website.siteContactLinkedIn} alt="LinkedIn">
+            <LinkedIn />
+          </ExternalLink>
+        </li>
         {/if}
         <!-- <li>
               <ExternalLink href="https://www.linkedin.com/company/tracer-canada" alt="Twitter">
@@ -89,7 +85,7 @@
     </div>
     <!-- -->
     <div>
-      <div class="link">
+      <div class=' link'>
         <!-- <a href="https://tracercanada.b2clogin.com/tracercanada.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_susi&client_id=5ac16849-77f9-4b75-ad57-90c114601daf&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fapp.tracercanada.ca%2F&scope=openid&response_type=code&prompt=login&code_challenge_method=S256&code_challenge=IKmGZ5UtSNtu_89im46O5ZmLB25xZmFMEDLGIkezNFo"
         target="_blank" alt="Sign In to app.tracercanada.ca">
         <button class="mdc-button mdc-button--outlined smui-button--color-primary ">
@@ -97,28 +93,24 @@
           <span class="mdc-button__label">SIGN IN</span>
         </button>
       </a> -->
-        <a
-          href={"mailto:" + website.siteContactEmail}
-          alt={"Sent mail to " + website.siteContactEmail}
-        >
-          <!-- reserve -->
-          <button
-            class=" mdc-button mdc-button--outlined smui-button--color-primary "
-          >
-            <div class=" mdc-button__ripple" />
-            <span class="mdc-button__label"
-              >{desktop ? "BOOK AN EVENT/RESERVATION" : "RESERVE"}</span
-            >
-          </button>
-        </a>
-        <!-- <button class="mdc-button mdc-button--outlined smui-button--color-secondary ">
+        <a href={"mailto:"+website.siteContactEmail} alt={"Sent mail to "+website.siteContactEmail}>
+
+        <!-- reserve -->
+        <button class=" mdc-button mdc-button--outlined smui-button--color-primary ">
+          <div class=" mdc-button__ripple">
+    </div>
+    <span class="mdc-button__label hidden">{desktop?"BOOK AN EVENT/RESERVATION":"RESERVE"}</span>
+    </button>
+
+    </a>
+    <!-- <button class="mdc-button mdc-button--outlined smui-button--color-secondary ">
       <div class="mdc-button__ripple"></div>
       <div id='google_translate_element' style="z-index:2332323232323;position:relative;"></div>
     </button> -->
-      </div>
-    </div>
-    <!-- Hacky Google Translate -->
-    <!-- <script type='text/javascript'
+  </div>
+  </div>
+  <!-- Hacky Google Translate -->
+  <!-- <script type='text/javascript'
   src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'></script>
 <script type='text/javascript'>
   function googleTranslateElementInit() {
