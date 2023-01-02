1182911b50891144255099ab449936c591dbe8e1
Ryan Spice : Book <spice.ryan@hotmail.com>
2023-01-02 11:28:18 -0500

    variable aimage grid, landing, logo

--- src/lib/components/static/Logo.svelte
+++ src/lib/components/static/Logo.svelte
@@ -1,14 +1,18 @@
-<script>
-  const imgAlt = "Logo";
-</script>
-
-<a href="https://tracercanada.ca/" class="main-logo  animated">
-  <img src="/images/logo_red_transparent.png" alt={imgAlt} />
-</a>
-
 <style>
   svg {
     margin-top: -150px;
     margin-bottom: -150px;
   }
+
+  .main-logo {
+    //margin: 35px 0 5px;
+  }
 </style>
+<script>
+  const imgAlt = "Logo";
+  const enabled = false;
+</script>
+
+<a href="https://tracercanada.ca/" class="main-logo  animated">
+  <img src="/images/logo_og_transparent.png" alt={imgAlt} />
+</a>
