df8f4104249eb24e0c8b073bc36de5b639e40709
Ryan Spice : Book <spice.ryan@hotmail.com>
2023-01-19 20:27:54 -0500

    replace hardcoded values for address, use vicinity.

--- src/lib/components/Address.svelte
+++ src/lib/components/Address.svelte
@@ -2,12 +2,15 @@
 	import { place } from "../stores.js";
 	let address;
 	place.subscribe((value) => {
-		address = value?.formatted_address;
+		// address = value?.formatted_address;
+		address = value?.vicinity;
+		console.log("Address.svelte :: place.subscribe :: ", value);
 	});
 </script>
 
 <div class="address w-100 center">
-	{address?.split(", London, ")[0].replace("St", "St.") || ""}
+	{address}
+	<!-- {address?.split(", London, ")[0].replace("St", "St.") || ""} -->
 </div>
 
 <style>
